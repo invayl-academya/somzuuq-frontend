04a7f9187db48d2d5dc9084a7815d52fbd9a2960
unknown <ymcsoln420@gmail.com>
2025-08-31 16:10:38 +0300

    added api

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,3 +1,10 @@
-import client from "@/api/client";
+import axios from "axios";
 
-export const APP_URL = client;
+export const API = import.meta.env.VITE_API_URL || "http://localhost:5000"; // fallback for local dev
+
+const APP_URL = axios.create({
+  baseURL: API,
+  withCredentials: true,
+});
+
+export default APP_URL;

8ad90fe173772bdee1200d32d8fc6afe4ef84e15
unknown <ymcsoln420@gmail.com>
2025-09-01 11:48:41 +0300

    connect api

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,1 +1,8 @@
-export const APP_URL = "http:..localhost:8000/api";
+// export const APP_URL = "https://clownfish-app-8irth.ondigitalocean.app/";
+
+const ROOT = (import.meta.env.VITE_API_URL || "http://localhost:8000").replace(
+  /\/+$/,
+  ""
+);
+
+export const APP_URL = `${ROOT}/api`;
